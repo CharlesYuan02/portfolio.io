0eac3df149f5bda7366d8de965310bd88bae9f3d
Charles Yuan <charlesyuan59@gmail.com>
2024-05-15 16:14:11 -0400

    Added get_users function

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,21 @@ import os
 import supabase
 
 
+def get_users():
+    '''
+    Retrieves all the users from the users table in Supabase.
+
+    Returns:
+        users (list): A list of all the users
+    '''
+    SUPABASE_URL = os.environ.get("SUPABASE_URL")
+    SUPABASE_API_KEY = os.environ.get("SUPABASE_API_KEY")
+    ALL_USERS_TABLE = os.environ.get("ALL_USERS_TABLE")
+    client = supabase.create_client(SUPABASE_URL, SUPABASE_API_KEY)
+    response = client.table(ALL_USERS_TABLE).select("*").execute()
+    return [row["email"] for row in response.data]
+
+
 def get_portfolios(email):
     '''
     Retrieves all the portfolios for a given user.
