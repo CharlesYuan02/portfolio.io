7c7147085fb9014a10005f7104d5ca55f4d08eff
Charles Yuan <charlesyuan59@gmail.com>
2024-05-16 13:09:04 -0400

    Added new function to get existing tickers from pymongo

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,10 @@
 import os
+import pymongo
 import supabase
+import logging
+
+# Suppress the "Waiting for suitable server to become available" error pymongo logs
+logging.getLogger('pymongo').setLevel(logging.WARNING)
 
 
 def get_users():
@@ -34,3 +39,25 @@ def get_portfolios(email):
     client = supabase.create_client(SUPABASE_URL, SUPABASE_API_KEY)
     response = client.table(PORTFOLIOS_TABLE).select("portfolio", "is_public").eq("email", email).execute()
     return [row["portfolio"] for row in response.data], {row["portfolio"]: row["is_public"] for row in response.data}
+
+
+def get_tickers():
+    '''
+    Retrieves all unique stock tickers from MongoDB.
+
+    Args:
+        None
+
+    Returns:
+        tickers (list): A list of all unique stock tickers
+    '''
+    # Connect to MongoDB Atlas
+    CLUSTER_NAME = os.environ.get("CLUSTER_NAME")
+    DB_NAME = os.environ.get("DB_NAME")
+    COLLECTION_NAME = os.environ.get("COLLECTION_NAME")
+    pymongo_client = pymongo.MongoClient(CLUSTER_NAME)
+    collection = pymongo_client[DB_NAME][COLLECTION_NAME]
+
+    # Get all unique stock tickers
+    tickers = collection.distinct("ticker")
+    return tickers
