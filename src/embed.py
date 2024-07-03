ad426e7f3ee2a0be7fe3b7124a9254baf50c8b26
Charles Yuan <charlesyuan59@gmail.com>
2024-07-03 15:39:57 -0400

    Switched from MongoDB to Pinecone

--- src/embed.py
+++ src/embed.py
@@ -3,16 +3,13 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_mongodb import MongoDBAtlasVectorSearch
-from tqdm import tqdm
-from src.utils import get_mongo_collection
+from langchain_pinecone import PineconeVectorStore
 
 
 def generate_embeddings(text, ticker, source, chunk_size=1500, chunk_overlap=500):
     '''
     Given the text extracted from a company's 10-K or 10-Q, 
-    use OpenAI to generate the vector embeddings and store in 
-    MongoDB Atlas Vector Store.
+    use OpenAI to generate the vector embeddings and store in Pinecone.
     
     Args:
         text (str): The text extracted from the company's 10-K or 10-Q
@@ -25,33 +22,29 @@ def generate_embeddings(text, ticker, source, chunk_size=1500, chunk_overlap=500
         embeddings (list): A list of the embeddings generated
     '''
 
-    # Connect to MongoDB Atlas
+    # Connect to Pinecone
     load_dotenv()
-    ATLAS_VECTOR_SEARCH_INDEX_NAME = os.environ.get("ATLAS_VECTOR_SEARCH_INDEX_NAME")
-    collection = get_mongo_collection()
+    os.environ['PINECONE_API_KEY'] = os.getenv("PINECONE_API_KEY")
 
     # Split the text into smaller chunks and embed
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size, chunk_overlap)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     texts = text_splitter.split_text(text)
 
-    for text in tqdm(texts):
-        MongoDBAtlasVectorSearch.from_texts(
-            [text], # You have to pass in a list or else it will not work properly
-            embedding=OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY")),
-            metadatas=[
-                {
-                    "ticker": ticker,
-                    "source": source,
-                }
-            ],
-            collection=collection,
-            index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME,
-        )
+    metadata = []
+    for text in texts:
+        metadata.append({"ticker": ticker, "source": source, "text": text})
+
+    PineconeVectorStore.from_texts(
+        texts,
+        embedding=OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY")),
+        metadatas=metadata,
+        index_name=os.getenv("VECTOR_SEARCH_INDEX"),
+    )
 
 
 if __name__ == "__main__":
-    ticker = "MSFT"
-    source = "10-Q"
+    ticker = "AAPL"
+    source = "10-K"
     NAME = os.environ.get("NAME")
     EMAIL = os.environ.get("EMAIL")
     edgar.set_identity(NAME + " " + EMAIL)
--- src/query.py
+++ src/query.py
@@ -2,13 +2,12 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import OpenAI, OpenAIEmbeddings
 from langchain.chains import RetrievalQA
-from langchain_mongodb import MongoDBAtlasVectorSearch
-from src.utils import get_mongo_collection
+from langchain_pinecone import PineconeVectorStore
 
 
 def vector_search(ticker, query):
     '''
-    Performs vector search on MongoDB Atlas vector store to retrieve relevant embeddings
+    Performs vector search on Pinecone to retrieve relevant embeddings
     while filtering results based on ticker specified.
     Uses OpenAI's gpt-3.5-turbo to generate a response given the retrieved embeddings.
 
@@ -20,14 +19,13 @@ def vector_search(ticker, query):
         retriever_output (str): The chatbot's answer.
     '''
     load_dotenv()
-    ATLAS_VECTOR_SEARCH_INDEX_NAME = os.getenv("ATLAS_VECTOR_SEARCH_INDEX_NAME")
-    collection = get_mongo_collection()
+    os.environ['PINECONE_API_KEY'] = os.getenv("PINECONE_API_KEY")
 
     # Define the filter based on the metadata field and value
-    vector_search = MongoDBAtlasVectorSearch(
+    vector_search = PineconeVectorStore(
         embedding=OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY")),
-        collection=collection,
-        index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME,
+        pinecone_api_key=os.getenv("PINECONE_API_KEY"),
+        index_name=os.getenv("VECTOR_SEARCH_INDEX"),
     )
 
     llm = OpenAI(
@@ -35,7 +33,7 @@ def vector_search(ticker, query):
         temperature=0,
     )
     
-    retriever = vector_search.as_retriever()
+    retriever = vector_search.as_retriever(search_kwargs={"k": 5, "filter": {"ticker": ticker}})
     qa = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
@@ -44,7 +42,7 @@ def vector_search(ticker, query):
     
     # Inject ticker into query to filter documents
     retriever_output = qa.invoke({"query": ticker + ": " + query})["result"]
-    return retriever_output.replace("$", "\$") # Escape dollar signs to prevent LaTeX rendering issues
+    return retriever_output.replace("$", "\$").lstrip() # Escape dollar signs to prevent LaTeX rendering issues
     
 if __name__ == "__main__":
     print(vector_search("AAPL", "How much did Americas net sales decrease in 2023 compared to Europe?"))
