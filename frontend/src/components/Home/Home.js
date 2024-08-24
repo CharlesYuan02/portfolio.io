7a30b13a7980c03f2940d96e3144705a9b17e52c
Charles Yuan <charlesyuan59@gmail.com>
2024-08-23 21:57:40 -0400

    Fixed Login button size issue, added Auth0 signup/login

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,16 +1,20 @@
 import './Home.css';
 import stonksImage from '../../assets/stonks.png';
-
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Home = () => {
+    const { loginWithRedirect } = useAuth0();
+
     return (
         <div className="content">
             <div className="text-content">
                 <h1 className="text-intro">My Portfolio is the Best...</h1>
                 <h1 className="text-intro">And Now I Can <em>Prove It!</em></h1>
-                <a href="https://portfolio-io.streamlit.app/Sign%20Up">
-                    <button className="cta-button">Join Now</button>
-                </a>
+                <button className="cta-button" onClick={() => loginWithRedirect({
+                    authorizationParams: {
+                        screen_hint: 'signup'
+                    }
+                })}>Join Now</button>
             </div>
             <div className="image-content">
                 <img src={stonksImage} alt="Stonks" />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            redirectUri={window.location.origin}
+        >
+            <App />
+        </Auth0Provider>
+    </React.StrictMode>
 );
