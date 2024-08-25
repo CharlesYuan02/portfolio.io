5bb003f84dd2a0b00c2ffcdfcc0565f1a53729a3
Charles Yuan <charlesyuan59@gmail.com>
2024-08-24 23:09:51 -0400

    Updated Home Signup button logic for creating username and checking if signed in

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,20 +1,56 @@
 import './Home.css';
 import stonksImage from '../../assets/stonks.png';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_API_KEY;
+const usersTable = process.env.REACT_APP_SUPABASE_USERS_TABLE;
+const supabase = createClient(supabaseURL, supabaseKey);
 
 const Home = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated, user } = useAuth0();
+    const { email, sub } = user || {};
+    const navigate = useNavigate();
+    
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
+        // Check if user has username in database
+        const { data: username, error: usernameError } = await supabase
+            .from(usersTable)
+            .select('username')
+            .eq('email', email)
+            .eq('password', sub);
+        if (usernameError) {
+            console.error('Error fetching username:', usernameError);
+            return;
+        }
+
+        if (username.length > 0) { // Redirect to dashboard if user has username
+            navigate('/dashboard');
+            return;
+        } else if (isAuthenticated) { // Redirect to create-username page if user is authenticated
+            navigate('/create-username');
+            return;
+        }
+        
+        // Remember to add /create-username to the allowed callback URLs in the Auth0 dashboard
+        loginWithRedirect({
+            authorizationParams: {
+                screen_hint: 'signup',
+                redirect_uri: `${window.location.origin}/create-username`
+            },
+        });
+    };
 
     return (
         <div className="content">
             <div className="text-content">
                 <h1 className="text-intro">My Portfolio is the Best...</h1>
                 <h1 className="text-intro">And Now I Can <em>Prove It!</em></h1>
-                <button className="cta-button" onClick={() => loginWithRedirect({
-                    authorizationParams: {
-                        screen_hint: 'signup'
-                    }
-                })}>Join Now</button>
+                <button className="cta-button" onClick={handleSignUp}>Join Now</button>
             </div>
             <div className="image-content">
                 <img src={stonksImage} alt="Stonks" />
