8c5fb77ddc74795516ab6d2c5c92ec639d10c957
Charles Yuan <charlesyuan59@gmail.com>
2024-08-24 23:09:23 -0400

    Updated Navbar login button logic for creating username and checking if signed in

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,8 +1,51 @@
 import './Navbar.css';
 import { useAuth0 } from '@auth0/auth0-react';
+import { createClient } from '@supabase/supabase-js';
+import { useNavigate } from 'react-router-dom';
+
+const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_API_KEY;
+const usersTable = process.env.REACT_APP_SUPABASE_USERS_TABLE;
+const supabase = createClient(supabaseURL, supabaseKey);
 
 const Navbar = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, user } = useAuth0();
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        if (!user) {
+            loginWithRedirect({
+                redirectUri: `${window.location.origin}/create-username`
+            });
+            return;
+        }
+
+        const { email, sub } = user;
+
+        // If user is logged in and has a username, redirect to dashboard
+        if (email && sub) {
+            // Check if user has username in database
+            const { data: username, error: usernameError } = await supabase
+                .from(usersTable)
+                .select('username')
+                .eq('email', email)
+                .eq('password', sub);
+            if (usernameError) {
+                console.error('Error fetching username:', usernameError);
+                return;
+            }
+
+            if (username.length > 0) {
+                navigate('/dashboard');
+                return;
+            } else {
+                navigate('/create-username');
+                return;
+            }
+        }
+    }
 
     return (
         <nav className="navbar">
@@ -10,10 +53,7 @@ const Navbar = () => {
             <div className="login">
                 <button 
                     className="no-style" 
-                    onClick={(e) => {
-                        e.preventDefault();
-                        loginWithRedirect();
-                    }}
+                    onClick={handleLogin}
                 >
                     Login
                 </button>
