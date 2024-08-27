b536502d3853a401c8e4337ce62e2fcb87cd9e6d
Charles Yuan <charlesyuan59@gmail.com>
2024-08-26 22:51:05 -0400

    Added basic outline for Dashboard to test Add Position menu

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,11 +1,30 @@
+import React, { useState } from 'react';
+import AddPosition from '../AddPosition/AddPosition';
 import './Dashboard.css';
 
 const Dashboard = () => {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const togglePopup = () => {
+        setIsPopupOpen(!isPopupOpen);
+    };
+
     return (
-        <div>
+        <div className="dashboard-container">
             <h1>Dashboard</h1>
+            
+            <button className="open-popup-btn" onClick={togglePopup}>
+                Add Position
+            </button>
+
+            <div className={`add-position-popup ${isPopupOpen ? 'open' : ''}`}>
+                <button className="close-popup-btn" onClick={togglePopup}>
+                &times;
+                </button>
+                <AddPosition />
+            </div>
         </div>
     );
-}
+};
 
 export default Dashboard;
