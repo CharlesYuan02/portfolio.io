870c6705b34ed36236014ed609eaea32c76740ad
Charles Yuan <charlesyuan59@gmail.com>
2024-09-09 22:48:43 -0400

    Added two scripts from previous Streamlit app for RAG chatbot feature

---
+++ backend/rag/chatbot.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAI, OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain_pinecone import PineconeVectorStore
+
+
+def vector_search(ticker, query):
+    '''
+    Performs vector search on Pinecone to retrieve relevant embeddings
+    while filtering results based on ticker specified.
+    Uses OpenAI's gpt-3.5-turbo to generate a response given the retrieved embeddings.
+
+    Args:
+        ticker (str): The stock ticker (e.g. AAPL) specified for filtering.
+        query (str): The question inputted by the user.
+
+    Returns:
+        retriever_output (str): The chatbot's answer.
+    '''
+    load_dotenv()
+    os.environ['PINECONE_API_KEY'] = os.getenv("PINECONE_API_KEY")
+
+    # Define the filter based on the metadata field and value
+    vector_search = PineconeVectorStore(
+        embedding=OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY")),
+        pinecone_api_key=os.getenv("PINECONE_API_KEY"),
+        index_name=os.getenv("VECTOR_SEARCH_INDEX"),
+    )
+
+    llm = OpenAI(
+        openai_api_key=os.getenv("OPENAI_API_KEY"),
+        temperature=0,
+    )
+    
+    retriever = vector_search.as_retriever(search_kwargs={"k": 5, "filter": {"ticker": ticker}})
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+    )
+    
+    # Inject ticker into query to filter documents
+    retriever_output = qa.invoke({"query": ticker + ": " + query})["result"]
+    return retriever_output.replace("$", "\$").lstrip() # Escape dollar signs to prevent LaTeX rendering issues
