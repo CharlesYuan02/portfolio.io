93f2447f74bd722f4d6d32ff0b329d9e66626e84
Charles Yuan <charlesyuan59@gmail.com>
2024-09-22 00:47:15 -0400

    Removed unnecessary CORS origin

--- backend/settings.py
+++ backend/settings.py
@@ -54,10 +54,7 @@ MIDDLEWARE = [
 
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = False  # Set this to False for production
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "backend.urls"
